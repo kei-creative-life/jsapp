30ee1203cdce10196fb005a43bf74a91fba1c508
kei1992 <68884990+kei1992@users.noreply.github.com>
2020-10-18 21:22:59 +0900

    リスト追加・ボタン反応

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // button(完了)タグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+  // button(削除)
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // divの子要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // // ulのしたにdivを追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
